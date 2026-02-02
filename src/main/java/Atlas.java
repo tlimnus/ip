d0359a3714dca4c5d1161b37acf44d2744b2bae4
wheatfieldone-admin <wheatfieldone@gmail.com>
2026-02-02 19:41:20 +0800

    Mark/Unmark

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Atlas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Task> li = new ArrayList<Task>();
 
         String logo =
                 "   ___  _______    ___    _____ \n"
@@ -14,11 +16,39 @@ public class Atlas {
 
         while(true){
             String input = sc.nextLine();
+
+            // |Echo+List| _______________________________________________________
+
             if (input.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            if (input.equals("list")){
+                for (Task text : li){
+                    System.out.println("[" + text.getStatusIcon() + "] " + text.description);
+                }
+                continue;
+            }
+
+            // |Mark or Unmark| ____________________________________________________________
+
+            if (input.startsWith("mark ") || input.startsWith("unmark ")){
+                String[] parts = input.trim().split("\\s+");
+                String num = parts[1];
+                int index = Integer.parseInt(num) - 1;
+                li.get(index).markAsDone();
+                if (input.startsWith("u")){ // Assumes user doesn't mark something that is already marked and vice versa
+                    System.out.println("OK, I've marked this task as not done yet:");
+                }
+                else{
+                    System.out.println("Nice! I've marked this task as Done:");
+                }
+                System.out.println("[" + li.get(index).getStatusIcon() + "] " + li.get(index).description);
+                continue;
+            }
+            li.add(new Task(input));
+            System.out.println("added: " + input);
+
         }
 
         sc.close();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String description;
+    protected Boolean isDone;
+
+    public Task (String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone () {
+        if (this.isDone){
+            this.isDone = false;
+            return;
+        }
+        this.isDone = true;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "x" : " ");
+    }
 }
