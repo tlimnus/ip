d7e8a0cb78e43066771cde832715cd82b35e873c
wheatfieldone-admin <wheatfieldone@gmail.com>
2026-02-20 08:43:18 +0800

    Save File

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -14,4 +14,9 @@ public class Events extends Task{
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    @Override
+    public String toFileFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+    }
 }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Storage {
+    private String filepath;
+
+    public Storage(String filepath){
+        this.filepath = filepath;
+    }
+
+    public void load() throws IOException {
+        try {
+            File file = new File(filepath);
+            List<Task> tasksi = new ArrayList<>();
+            Scanner sc = new Scanner(file);
+            if(file.createNewFile()){
+                System.out.println("File created and loaded.");
+            } else {
+                System.out.println("File loaded.");
+            }
+            while(sc.hasNext()){
+                String line = sc.nextLine();
+                String[] parts = line.trim().split("|");
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
+    }
+}
